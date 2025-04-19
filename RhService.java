53456f1779ff43a4a1c3348d9690855b5bef60af
Ayrton Nhaguaga <ayrtonjuliojaimenhaguaga@gmail.com>
2025-04-18 21:56:12 -1200

    Add files via upload

--- RhService.java
+++ RhService.java
@@ -30,4 +30,58 @@ public class RhService {
 	public List<Funcionario> mostrarFuncionarios(){
 		return funcionarios;
 	}
+	
+	public boolean registrarFaltaPorId(int id, int quantidade) {
+		Optional<Funcionario> funcionario = pesquisarPorId(id);
+		if (funcionario.isPresent()) {
+			funcionario.get().registrarFalta(quantidade);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean registrarFaltaPorNome(String nome, int quantidade) {
+		Optional<Funcionario> funcionario = pesquisarPorNome(nome);
+		if (funcionario.isPresent()) {
+			funcionario.get().registrarFalta(quantidade);
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean registrarFaltaPorBi(String bi, int quantidade) {
+		Optional<Funcionario> funcionario = pesquisarPorNome(bi);
+		if (funcionario.isPresent()) {
+			funcionario.get().registrarFalta(quantidade);
+			return true;
+		}
+		return false;
+	}
+	
+	public void mostrarFaltasDeTodos() {
+		if (funcionarios.isEmpty()) {
+			System.out.println("Nenhum funcionario cadastrado.");
+			return;
+		}
+		System.out.println("Faltas por funcionario: ");
+		for (Funcionario f : funcionarios) {
+			System.out.println("ID: "+f.getId()+" | Nome: "+f.getNome() + " | Faltas: "+f.getFaltas());
+		}
+	}
+	
+	public double calcularMediaFaltas() {
+		if (funcionarios.isEmpty()) {
+			return 0.0;
+		}
+		int totalFaltas = 0;
+		for (Funcionario f : funcionarios) {
+			totalFaltas += f.getFaltas();
+		}
+		return (double) totalFaltas / funcionarios.size();
+		
+	}
+	
+	public Optional<Funcionario> funcionarioComMaisFaltas() {
+		return funcionarios.stream().max((f1, f2) -> Integer.compare(f1.getFaltas(), f1.getFaltas()));
+	}
 }
